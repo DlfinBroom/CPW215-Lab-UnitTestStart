f48d2cac0ebe57791931b759c494b190b698defc
lampert309 <lampert309@gmail.com>
2019-04-29 14:25:38 -0700

    Fixed course class to pass all courseTest unit tests

--- BusinessLogic/Course.cs
+++ BusinessLogic/Course.cs
@@ -6,25 +6,64 @@ namespace BusinessLogic
 {
     public class Course
     {
+        private string _instructorName;
+        private string _courseName;
+        private byte _numberOfCredits;
+
         public Course(string courseName)
         {
             CourseName = courseName;
+            InstructorName = "STAFF";
         }
 
         /// <summary>
         /// The instructor teaching the course
         /// </summary>
-        public string InstructorName { get; set; }
+        public string InstructorName {
+            get {
+                return _instructorName;
+            }
+            set {
+                _instructorName = value;
+            }
+        }
 
         /// <summary>
         /// The name of the course
         /// </summary>
-        public string CourseName { get; set; }
+        public string CourseName {
+            get {
+                return _courseName;
+            }
+            set {
+                if (value != null)
+                {
+                    _courseName = value;
+                }
+                else
+                {
+                    throw new ArgumentNullException();
+                }
+            }
+        }
 
         /// <summary>
         /// Number of credits for the course
         /// </summary>
-        public byte NumberOfCredits { get; set; }
-
+        public byte NumberOfCredits {
+            get {
+                return _numberOfCredits;
+            }
+            set {
+                if (value < 30 && value > 0)
+                {
+                    _numberOfCredits = value;
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
     }
 }
